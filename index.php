c20f0d3aaf2e6905cf4d6c0eed45d972574ec849
unknown <deyoucyril35@gmail.com>
2024-08-03 12:26:13 +0200

    project structure created

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Marrigae verification system</title>
+</head>
+<body>
+    <h1> marrigae verification ssystem</h1>
+    <h3>start cooding</h3>
+</body>
+</html>
